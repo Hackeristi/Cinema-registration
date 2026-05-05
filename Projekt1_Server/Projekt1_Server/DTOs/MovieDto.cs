762c24a5fcc9b75211579c5823bbeca3ec1745f8
karichil <karina.chilkiewicz@gmail.com>
2026-05-05 21:47:42 +0200

    połączenie z mainem...

--- Projekt1_Server/Projekt1_Server/DTOs/MovieDto.cs
+++ Projekt1_Server/Projekt1_Server/DTOs/MovieDto.cs
@@ -3,7 +3,6 @@ public class MovieDto
 {
     public int MovieId { get; set; }
     public int ShowId { get; set; }
-    public int MovieId { get; set; }
     public string Title { get; set; }
     public string Genre { get; set; }
     public DateTime ShowDatetime { get; set; }
