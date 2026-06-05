0a68160a3d82e0e31e9bc5e40cf94f107509644f
Klaudia Natalia Klim <128401556+Hackeristi@users.noreply.github.com>
2026-06-05 13:26:19 +0200

    Dodano nowe foldery, zmieniono nazwy

---
+++ REST/client-java/server-dotnet/Projekt1_Server/DTOs/ReservationDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Projekt1_Server.DTOs;
+
+public class ReservationDto
+{
+    public int ReservationId { get; set; }
+    public int FilmShowId { get; set; }
+    public string Title { get; set; }
+    public DateTime ShowDatetime { get; set; }
+    public List<string> TakenSeats { get; set; }
+}
